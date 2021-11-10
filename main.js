83da47c75be5f554447a6aa35cac7c68425f2062
Joel K <duveled@gmail.com>
2021-11-10 16:22:01 +0100

    fetch kurser. delete fungerar ej

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-console.log("hello");
 let output = document.getElementById("output");
-
+// ladda listan
+window.addEventListener("load", getCourses());
 // hämta kurser
-function getCourse() {
-  output.innerHTML = "";
+function getCourses() {
+  //   output.innerHTML = "";
   fetch("http://localhost:5000/api/courses")
     .then((res) => res.json())
     .then((data) => {
       data.forEach(function (course) {
         console.log(course);
+        output.innerHTML += `
+        <tr>
+            <td>${course.id}</td>
+            <td>${course.courseId}</td>
+            <td>${course.courseName}</td>
+            <td>${course.coursePeriod}</td>
+            <td><button onClick="deleteCourse(${course.id})">Radera</button></td>
+        </tr>
+        `;
       });
     });
 }
 
-console.log(getCourse());
+// ta bort
+function deleteCourse(id) {
+  fetch("http://localhost:5000/api/courses?id=" + id, { method: "DELETE" })
+    .then((res) => res.json())
+    .then((data) => {
+      getCourses();
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+    });
+}
